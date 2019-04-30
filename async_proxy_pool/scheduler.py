fa2d91cf34397b78c4743bc62805acc8be41c032
张杰 <jie.zhang8@luckyair.net>
2019-04-30 15:16:14 +0800

    fix the bug that mem usage increase unlimited

--- async_proxy_pool/scheduler.py
+++ async_proxy_pool/scheduler.py
@@ -6,6 +6,7 @@ from multiprocessing import Process
 
 from async_proxy_pool.config import SERVER_HOST, SERVER_PORT, SERVER_ACCESS_LOG
 from async_proxy_pool.webapi_sanic import app
+from async_proxy_pool.logger import logger
 from .config import CRAWLER_RUN_CYCLE, VALIDATOR_RUN_CYCLE
 from .crawler import crawler
 from .validator import validator
@@ -20,18 +21,21 @@ class Scheduler:
     def crawler_task(cycle=CRAWLER_RUN_CYCLE):
         while True:
             crawler.run()
-            time.sleep(cycle*60)
+            time.sleep(cycle * 60)
 
     @staticmethod
     def validator_task(cycle=VALIDATOR_RUN_CYCLE):
         while True:
             validator.run()
-            time.sleep(cycle*60)
+            time.sleep(cycle * 60)
 
     def run(self):
-        api_process = Process(target=Scheduler.api)
-        api_process.start()
-        crawler_process = Process(target=Scheduler.crawler_task)
-        crawler_process.start()
-        validator_process = Process(target=Scheduler.validator_task)
-        validator_process.start()
+        try:
+            api_process = Process(target=Scheduler.api)
+            api_process.start()
+            crawler_process = Process(target=Scheduler.crawler_task)
+            crawler_process.start()
+            validator_process = Process(target=Scheduler.validator_task)
+            validator_process.start()
+        except KeyboardInterrupt as e:
+            logger.info("You have canceled all jobs")
